600f38bd1e6e3c64b2add1083cd004a5065ff0a6
HarshUdai <harshudai2001@gmail.com>
2021-01-12 11:33:53 +0530

    Initial Commit

---
+++ Random_No/randomNumber.py
@@ -0,0 +1,12 @@
+# This is the simple programe to generate the random number.
+# Random Library is used
+import random
+
+# In random library, we have randint function,
+# in which we have 2 parameters.
+# therefore it will give any no between these 2.
+
+number = random.randint(1,1000)
+
+# Printing the number
+print("Random Number ->",number)
